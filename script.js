0145f0a15c9e8b03b4e45451e88a3e3b4299a243
ibrataha8 <ibrataha8@gmail.com>
2023-02-12 19:43:33 +0100

    Finish Programm

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const DAYS_OF_WEEK = 7; //i
-const MONTHS = 12; //j
+const DAYS_OF_WEEK = 8; //i
+const MONTHS = 10; //j
 const WEEKS_IN_MONTHS = 4; //k
 const cont = document.querySelector(".cont");
 const MONTH_NAMES = [
@@ -13,31 +13,32 @@ const MONTH_NAMES = [
   "Aug",
   "Sep",
   "Oct",
-  "Nov",
-  "Dec",
+  // "Nov",
+  // "Dec",
 ];
-const WEEKS_NAMES = ["Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"];
+const WEEKS_NAMES = ["","Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"];
 let row = "";
-let l = 0
+let l = 0;
 // Loop over days
 for (let i = 0; i < DAYS_OF_WEEK; i++) {
-  row += "<div class='row'>";
-  // Loop over month
-
-  let month = WEEKS_NAMES[i];
-  row += "<div class='day-name'>" + month + "</div>";
-  for (var j = 0; j < MONTHS; j++) {
-    // Loop over each day
-    if (j % 2 !== 0 && i ==0) {
+  if (i == 0) {
+    for (var j = 0; j < MONTHS; j++) {
       let month = MONTH_NAMES[l++];
-      row += "<div class='month-name'>" + month + "</div>";
+      row += "<div class='month-name'>"+"&nbsp;".repeat(MONTHS-i) + month  + "</div>";
     }
-
-    for (let k = 0; k < WEEKS_IN_MONTHS; k++) {
-      row += "<span class='square' >&nbsp;&nbsp;&nbsp;&nbsp;</span>";
+  } else {
+    row += "<div class='row'>";
+    // Loop over month
+    let month = WEEKS_NAMES[i];
+    row += "<div class='day-name'>" + month + "</div>";
+    for (var j = 0; j < MONTHS; j++) {
+      // Loop over each day
+      for (let k = 0; k < WEEKS_IN_MONTHS; k++) {
+        row += "<span class='square' >&nbsp;&nbsp;&nbsp;&nbsp;</span>";
+      }
     }
+    row += "</div>";
   }
-  row += "</div>";
 }
 console.log(row);
 cont.innerHTML = row;
